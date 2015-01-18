36eba9b1155e3e34da1c0db4c00315d854b53d30
Kyungmin Kim <kmin905@gmail.com>
2015-01-17 18:31:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Tipster
+
+This is a pre-work for the Swift class by Codepath. Tipster allows you to enter the bill amount and tip percentage to get the total amount.
+
+**How many hours did it take to complete?**
+2 hours
+
+
+**Which required and optional stories have you completed?**
+- [x] Required: Tip Calculator application
+- [x] Optional: Animating View Properties
+- [] Optional: Adding Image Assets
+
+**GIF walkthrough**
+![Walkthrough](/tipster.gif)
--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex];
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+        UIView.animateWithDuration(0.4, animations: {
+            self.billField.alpha = 1
+            self.billField.alpha = 0.5
+        })
+    }
+    @IBAction func onEdit(sender: UITextField) {
+        UIView.animateWithDuration(0.4, animations: {
+            self.billField.alpha = 0.5
+            self.billField.alpha = 1
+        })
+    }
 }
 
